74ccb71f2011b959a8960d7471ab5fbf5a6a3828
HungNg1012 <hung.nguyen1012@hcmut.edu.vn>
2022-12-10 09:25:52 +0700

    Done map using google api

--- src/pages/Dashboard/components/Map/index.js
+++ src/pages/Dashboard/components/Map/index.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "700px",
+  height: "400px",
+};
+
+const center = {
+  lat: 10.771871,
+  lng: 466.65787,
+};
 
 function Map() {
-  return <div>Map</div>;
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDNI_ZWPqvdS6r6gPVO50I4TlYkfkZdXh8",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
 export default Map;
